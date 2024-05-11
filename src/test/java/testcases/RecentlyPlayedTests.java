00b1bb5c7d619fbe89677c1e326118338737d062
xXFranzFXx <franz.fernando@testpro.io>
2024-05-10 19:56:33 -0700

    Update RecentlyPlayedTests.java

--- src/test/java/testcases/RecentlyPlayedTests.java
+++ src/test/java/testcases/RecentlyPlayedTests.java
@@ -32,12 +32,25 @@ public class RecentlyPlayedTests extends BaseTest {
         homePage.clickRecentlyPlayed();
         List<String> recentlyPlayedPageTitles = recentlyPlayedPage.recentlyPlayedSongs();
         Set<String> titles =  recentlyPlayedPageTitles.stream()
-                .flatMap(str -> 
+                .flatMap(str ->
                         homePageTitles.stream().filter(str2 ->
                                 str2.contains(str))).collect(Collectors.toSet());
         TestListener.logInfoDetails("Recently played song titles on homepage: " + homePageTitles);
         TestListener.logInfoDetails("Song titles on Recently Played page: " + recentlyPlayedPageTitles);
+        TestListener.logRsDetails("Actual songs listed: " + titles);
         TestListener.logAssertionDetails("Song titles are displayed correctly: " + homePageTitles.equals(recentlyPlayedPageTitles));
         Assert.assertNotEquals(recentlyPlayedPageTitles, homePageTitles);
     }
-}
+    @Test(description = "Verify number of songs in recently played section of homepage is the same as number of songs in recently played page")
+    public void recentlyPlayedSongs() {
+        setupRpPage();
+        List<String> homePageTitles = homePage.recentlyPlayedTitles();
+        homePage.clickRecentlyPlayed();
+        List<String> recentlyPlayedPageTitles = recentlyPlayedPage.recentlyPlayedSongs();
+
+        TestListener.logInfoDetails("Number of Recently played song titles on homepage: " + homePageTitles.size());
+        TestListener.logInfoDetails("Number of song titles on Recently Played page: " + recentlyPlayedPageTitles.size());
+        TestListener.logAssertionDetails("Number of song titles are equal: " + (homePageTitles.size() == recentlyPlayedPageTitles.size()));
+        Assert.assertEquals(recentlyPlayedPageTitles.size(), homePageTitles.size());
+    }
+    }
