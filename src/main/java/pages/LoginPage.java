90e89bd0853e367a90fe5b5df7174275dfb14276
xXFranzFXx <franz.fernando@testpro.io>
2024-06-10 16:51:19 -0700

    Update LoginPage.java

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.CacheLookup;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.ui.ExpectedConditions;
+
 
 public class LoginPage extends BasePage{
     HomePage homePage;
