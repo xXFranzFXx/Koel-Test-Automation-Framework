1d48f21063c90d88bbf255a78be6efee4f7da637
xXFranzFXx <franz.fernando@testpro.io>
2024-06-19 13:21:25 -0700

    Update FavoritesTests.java

--- src/test/java/testcases/FavoritesTests.java
+++ src/test/java/testcases/FavoritesTests.java
@@ -24,7 +24,7 @@ public class FavoritesTests extends BaseTest {
     LoginPage loginPage;
     AllSongsPage allSongsPage;
     FavoritesPage favoritesPage;
-    
+
     @AfterClass
     public void deleteDownloadedFiles() {
         TestUtil.deleteFiles();
