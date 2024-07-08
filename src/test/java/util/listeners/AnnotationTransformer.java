fd54a796285c0278b2e137338ad96bcb76353886
xXFranzFXx <franz.fernando@testpro.io>
2024-07-08 10:40:31 -0700

    Update AnnotationTransformer.java

--- src/test/java/util/listeners/AnnotationTransformer.java
+++ src/test/java/util/listeners/AnnotationTransformer.java
@@ -5,7 +5,6 @@ import org.testng.annotations.ITestAnnotation;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
-
 public class AnnotationTransformer implements IAnnotationTransformer {
     @Override
     public void transform(ITestAnnotation annotation, Class testClass, Constructor testConstructor, Method testMethod) {
