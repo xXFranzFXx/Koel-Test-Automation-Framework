395e2f79e6485677e81fafe304c2da816e874dc0
xXFranzFXx <franz.fernando@testpro.io>
2024-07-19 10:21:40 -0700

    Update RegistrationPage.java

--- src/main/java/pages/RegistrationPage.java
+++ src/main/java/pages/RegistrationPage.java
@@ -3,7 +3,6 @@ package pages;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-
 public class RegistrationPage extends BasePage{
    @FindBy(xpath = "//input[@type='email']")
    private WebElement emailInput;
