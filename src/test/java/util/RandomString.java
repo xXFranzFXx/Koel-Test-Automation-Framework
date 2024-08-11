2c5ddaedfb0f6c10725a51fdd9124ea9162304f4
xXFranzFXx <franz.fernando@testpro.io>
2024-08-10 19:09:34 -0700

    Update RandomString.java

--- src/test/java/util/RandomString.java
+++ src/test/java/util/RandomString.java
@@ -6,7 +6,6 @@ public class RandomString {
         String AlphaNumericString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
                 + "0123456789"
                 + "abcdefghijklmnopqrstuvxyz";
-
         StringBuilder sb = new StringBuilder(n);
         for (int i = 0; i < n; i++) {
             int index = (int)(AlphaNumericString.length() * Math.random());
