e7050097dcfae0b75d1ee08c304c8c0bdb84a148
xXFranzFXx <franz.fernando@testpro.io>
2024-08-11 22:49:55 -0700

    Update TestDataHandler.java

--- src/test/java/util/TestDataHandler.java
+++ src/test/java/util/TestDataHandler.java
@@ -5,11 +5,9 @@ import java.util.Map;
 
 public class TestDataHandler {
     Map<String, Object> testDataInMap = new HashMap<String, Object>();
-
     public Map<String, Object> getTestDataInMap() {
         return testDataInMap;
     }
-
     public void setTestDataInMap(Map<String, Object> testDataInMap) {
         this.testDataInMap = testDataInMap;
     }
