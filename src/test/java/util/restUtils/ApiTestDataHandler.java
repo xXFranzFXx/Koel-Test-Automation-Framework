eb08f5c9378cf3f0ce7129470b2d1d0b90bef2bd
xXFranzFXx <franz.fernando@testpro.io>
2024-08-14 11:48:16 -0700

    Update ApiTestDataHandler.java

--- src/test/java/util/restUtils/ApiTestDataHandler.java
+++ src/test/java/util/restUtils/ApiTestDataHandler.java
@@ -7,7 +7,6 @@ import java.util.Map;
 
 public class ApiTestDataHandler {
     Map<String, Object> testDataInMap=new HashMap<String, Object>();
-
     public String createPayload(String key, String value) {
         JSONObject jsonObject = new JSONObject();
         jsonObject.put(key, value);
@@ -31,7 +30,4 @@ public class ApiTestDataHandler {
     public void setApiTestDataInMap(Map<String, Object> testDataInMap) {
         this.testDataInMap = testDataInMap;
     }
-
-
-
 }
