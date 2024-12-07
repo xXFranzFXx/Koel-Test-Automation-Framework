93ccb4d01990710745684d99f982793dd06c9954
xXFranzFXx <franz.fernando@testpro.io>
2024-12-07 09:53:52 -0800

    Update KoelDbTests.java

--- src/test/java/testcases/KoelDbTests.java
+++ src/test/java/testcases/KoelDbTests.java
@@ -13,7 +13,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-
 public class KoelDbTests extends KoelDbActions {
     DbTestDataHandler testData = new DbTestDataHandler();
 
