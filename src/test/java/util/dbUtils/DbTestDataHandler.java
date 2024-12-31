ab989e333a7400f5ae6d2c156aacd12cdd911475
xXFranzFXx <franz.fernando@testpro.io>
2024-12-30 17:41:15 -0800

    Update DbTestDataHandler.java

--- src/test/java/util/dbUtils/DbTestDataHandler.java
+++ src/test/java/util/dbUtils/DbTestDataHandler.java
@@ -6,7 +6,6 @@ import java.util.Map;
 
 public class DbTestDataHandler {
     Map<String, ResultSet> testDataInMap = new HashMap<>();
-
     public Map<String, ResultSet> getTestDataInMap() {
         return testDataInMap;
     }
